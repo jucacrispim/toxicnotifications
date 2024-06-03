2d6c76c5c467ac880e0fbd93bebb3105bd9b06c1
Juca Crispim <jucacrispim@gmail.com>
2024-06-03 20:25:19 -0300

    upload path

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,4 +9,4 @@ cd dist
 fname=`ls | grep tar`
 project_name=toxicnotifications
 
-curl -F file=@$fname -F prefix=pypi/$project_name https://pypi.poraodojuca.dev/u/ -H "Authorization: Key $PYPI_AUTH_KEY"
+curl -F file=@$fname -F prefix=$project_name https://pypi.poraodojuca.dev/u/ -H "Authorization: Key $PYPI_AUTH_KEY"
